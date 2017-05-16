41af732e3b8142b62fe3ae0d6438f70f2e7811ac
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-16 12:24:31 +0300

    Write code to pass test_office_created_succesfully

--- amity.py
+++ amity.py
@@ -1,4 +1,9 @@
 class Amity:
     '''Definitionof Amity class
     '''
-    pass
+    def __init__(self):
+        self.all_rooms = []
+
+    def create_room(self, name, type):
+        self.all_rooms.append(name)
+        return "{} has been successfully created".format(name)
