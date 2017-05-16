dd3de3dc1b5c0a7343246b2c46165d19a13729b7
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-16 12:04:53 +0300

    Write test for room created succesfully

--- test_amity.py
+++ test_amity.py
@@ -2,10 +2,23 @@ import unittest
 from amity import Amity
 
 
-class TestAmityFunctions(unittest.TestcCases):
+class TestAmityFunctions(unittest.TestCase):
     '''Definiation of test cases for Amity functions
     '''
     def setUp(self):
         ''' Set up for the test cases
         '''
         self.amity = Amity()
+
+    def test_office_created_succesfully(self):
+        '''Tests if one office room is created succesfully using the create_room method
+        '''
+        initial_room_no = len(self.amity.all_rooms)
+        tsavo_office = self.amity.create_room("Tsavo", "office")
+        final_room_no = len(self.amity.all_rooms)
+        self.asssertTrue(tsavo_office)
+        self.assertEqual(1, initial_room_no - final_room_no)
+
+
+if __name__ == '__main__':
+    unittest.main()
