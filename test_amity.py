dc703d9974ed5e486b2aa6e281e0b4a4cd434e16
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-16 09:30:14 +0300

    Write set up method for the test cases

--- test_amity.py
+++ test_amity.py
@@ -1,1 +1,11 @@
+import unittest
+from amity import Amity
 
+
+class TestAmityFunctions(unittest.TestcCases):
+    '''Definiation of test cases for Amity functions
+    '''
+    def setUp(self):
+        ''' Set up for the test cases
+        '''
+        self.amity = Amity()
