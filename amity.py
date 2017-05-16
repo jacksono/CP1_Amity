2f64930975692042ac84166559f11cf60aaec40e
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-16 12:46:16 +0300

    Write code to pass test 3

--- amity.py
+++ amity.py
@@ -4,6 +4,6 @@ class Amity:
     def __init__(self):
         self.all_rooms = {}
 
-    def create_room(self, name, room_type):
-        self.all_rooms[name] = room_type
-        return "{} has been successfully created".format(name)
+    def create_room(self, room_name, room_type):
+        self.all_rooms[room_name] = room_type
+        return "{} has been successfully created".format(room_name)
