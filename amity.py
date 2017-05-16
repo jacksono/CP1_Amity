a195405300151640110d52a549366cc223436e21
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-16 12:38:42 +0300

    Write code to pass test case 2

--- amity.py
+++ amity.py
@@ -2,8 +2,8 @@ class Amity:
     '''Definitionof Amity class
     '''
     def __init__(self):
-        self.all_rooms = []
+        self.all_rooms = {}
 
-    def create_room(self, name, type):
-        self.all_rooms.append(name)
+    def create_room(self, name, room_type):
+        self.all_rooms[name] = room_type
         return "{} has been successfully created".format(name)
