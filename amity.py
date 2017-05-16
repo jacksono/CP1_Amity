b6aa30018cf15d4cbaef2d5e339b194c8f8f5aca
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-16 13:19:45 +0300

    Write code to pass test 4: test_can_create_multiple_rooms

--- amity.py
+++ amity.py
@@ -2,8 +2,13 @@ class Amity:
     '''Definitionof Amity class
     '''
     def __init__(self):
+        '''Initialiser for the Amity class
+        '''
         self.all_rooms = {}
 
     def create_room(self, room_name, room_type):
-        self.all_rooms[room_name] = room_type
+        '''function to create rooms given room names and room type
+        '''
+        for name in room_name:
+            self.all_rooms[name] = room_type
         return "{} has been successfully created".format(room_name)
