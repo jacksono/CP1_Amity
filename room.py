680b75aaa2fc627be20c4f1e74d04d4e59f135f2
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-18 06:06:24 +0300

    Add Office class

--- room.py
+++ room.py
@@ -13,3 +13,12 @@ class Room:
         self.room_type = room_type
         self.room_capacity = room_capacity
         self.rooms_list = []
+
+
+class Office(Room):
+    """Class to create office rooms."""
+
+    def __init__(self, name):
+        """Set Initialiser of the Room class."""
+        self.room_capacity = 6
+        super(Office, self).__init__(name, 'Office', self.room_capacity)
