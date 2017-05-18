c1ac9c3c23a550ff113c84119c6a8d2792ee6e57
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-18 11:01:15 +0300

    Modify code to use methods form the room classes

--- amity.py
+++ amity.py
@@ -1,4 +1,5 @@
 """Module to contain the amity class and methods  for all amity operations."""
+from room import Office, LivingSpace
 
 
 class Amity:
@@ -6,11 +7,16 @@ class Amity:
 
     def __init__(self):
         """Initialise  the Amity class."""
-        self.all_rooms = {}
+        self.amity_offices = {}
+        self.amity_living_spaces = {}
+        self.office = Office()
+        self.living_space = LivingSpace()
 
-    def create_room(self, room_name, room_type):
+    def create_room(self, room_name, type):
         """Create rooms given room names and room type."""
-        for name in room_name:
-            self.all_rooms[name] = room_type
-            print("{} has been created".format(name))
-        return "Command completed succesfully"
+        if type == 'o':
+            self.office.create_room(room_name)
+            self.amity_offices.update(self.office.all_rooms)
+        elif type == 'l':
+            self.living_space.create_room(room_name)
+            self.amity_living_spaces.update(self.living_space.all_rooms)
