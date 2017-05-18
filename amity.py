1d68a3bdc9969a3c105bf8bd5e71e4ef8f901d5a
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-18 09:56:39 +0300

    Rewrite docstrings to meet PEP8 standards

--- amity.py
+++ amity.py
@@ -1,14 +1,15 @@
+"""Module to contain the amity class and methods  for all amity operations."""
+
+
 class Amity:
-    '''Definition of Amity class
-    '''
+    """Definition of Amity class."""
+
     def __init__(self):
-        '''Initialiser for the Amity class
-        '''
+        """Initialise  the Amity class."""
         self.all_rooms = {}
 
     def create_room(self, room_name, room_type):
-        '''function to create rooms given room names and room type
-        '''
+        """Create rooms given room names and room type."""
         for name in room_name:
             self.all_rooms[name] = room_type
             print("{} has been created".format(name))
