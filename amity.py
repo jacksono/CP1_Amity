837930b7b833c2d55fbebdbe3aa6e5852231b94e
ONYANGO JACKSON JOHN <jxn@ONYANGOs-MacBook-Pro.local>
2017-05-26 20:15:50 +0300

    Add print messages when a person is created

--- amity.py
+++ amity.py
@@ -45,10 +45,15 @@ class Amity:
                 if len(self.amity_offices[allocated_room])\
                         < self.office.room_capacity:
                     self.amity_offices[allocated_room].append(person_name)
+                    return("{0} has been allocated to the Office: {1}".format(
+                                                  person_name, allocated_room))
                 else:
                     return ("Allocated room: {} is fully occupied."
                             " Please use reallocate to find a new room".format(
                                 allocated_room))
+            else:
+                return("There are currently no Offices to"
+                       " allocate {} to.".format(person_name))
         elif room_type == "l" or room_type == "L":
             if self.amity_living_spaces:
                 allocated_room = random.choice(list(
@@ -57,25 +62,35 @@ class Amity:
                         < self.living_space.room_capacity:
                     self.amity_living_spaces[allocated_room].append(
                                                                person_name)
+                    return("{0} has been allocated to the Living space:"
+                           " {1}".format(person_name, allocated_room))
                 else:
                     return ("Allocated room: {} is fully occupied."
                             " Please use reallocate to find a new room".format(
                                 allocated_room))
+            else:
+                return("There are currently no Living Spaces to"
+                       " allocate {} to.".format(person_name))
 
     def add_person(self, person_name, person_type, wants_acc=False):
         """Create people given name, type and accomodation option."""
-        if person_type == 'Staff':
+        if (person_name in self.amity_staff or
+                person_name in self.amity_fellows):
+            print("That name already exists, Please use another name")
+        elif person_type == 'Staff':
             self.staff.add_person(person_name)
             self.amity_staff.update(self.staff.all_people)
-            return self.allocate(person_name, "O")
+            print("{} has been created as a Staff".format(person_name))
+            print(self.allocate(person_name, "O"))
         elif person_type == 'Fellow':
             self.fellow.add_person(person_name, wants_acc)
             self.amity_fellows.update(self.fellow.all_people)
+            print("{} has been created as a Fellow".format(person_name))
             if not wants_acc:
-                return self.allocate(person_name, "O")
+                print(self.allocate(person_name, "O"))
             elif wants_acc:
-                self.allocate(person_name, "O")
-                return self.allocate(person_name, "L")
+                print(self.allocate(person_name, "O"))
+                print(self.allocate(person_name, "L"))
 
     def reallocate(self, person_name, new_room):
         """Reallocate people from old room to a new one."""
